507a49cf4f452a581933f84e793f50ba6d48d746
stephantul <stephantul@gmail.com>
2025-01-09 22:54:21 +0100

    prettify a little bit

--- semhash/index.py
+++ semhash/index.py
@@ -11,7 +11,16 @@ DictItem = list[dict[str, str]]
 
 class Index:
     def __init__(self, vectors: np.ndarray, items: list[DictItem], backend: AbstractBackend) -> None:
-        """Make the index."""
+        """
+        An index that maps vectors to items.
+
+        This index has an efficient backend for querying, but also explicitly stores the vectors in memory.
+
+        :param vectors: The vectors of the items.
+        :param items: The items in the index. This is a list of lists. Each sublist contains one or more dictionaries
+            that represent records. These records are exact duplicates of each other.
+        :param backend: The backend to use for querying.
+        """
         self.items = items
         self.backend = backend
         self.vectors = vectors
@@ -30,16 +39,16 @@ class Index:
 
     def query_threshold(self, vectors: np.ndarray, threshold: float) -> list[DocScores]:
         """Query the index with a threshold."""
-        out: list[list[tuple[dict[str, str], float]]] = []
+        out: list[DocScores] = []
         for result in self.backend.threshold(vectors, threshold=1 - threshold, max_k=100):
             intermediate = []
             for index, distance in zip(*result):
-                distances = []
-                items = []
-                for item in self.items[index]:
-                    distances.append(1 - distance)
-                    items.append(item)
-                intermediate.extend(list(zip(items, distances)))
+                # Every item in the index contains one or more records.
+                # These are all exact duplicates, so they get the same score.
+                for record in self.items[index]:
+                    # The score is the cosine similarity.
+                    # The backend returns distances, so we need to convert.
+                    intermediate.append((record, 1 - distance))
             out.append(intermediate)
 
         return out
